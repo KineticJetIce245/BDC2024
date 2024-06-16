a2b088ed83f16926f57c331ca84d6faf0b33aebe
Kineticjetice245 <kineticjetice245@gmail.com>
2024-06-15 23:15:49 -0400

    compute cusum for all countries with w = 0.5, standarized by dividing the maximum rate, and with expected mean and std from years before covid.

--- cusum.py
+++ cusum.py
@@ -2,22 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Cusum_cal :
-    def __init__(self, array: np.array, w = 0.5) -> None:
+
+class Cusum_cal:
+    def __init__(self, array: np.array, w=0.5) -> None:
         self.original_array = array
         self.__w_value = w
-    
+
     def set_w(self, w_value) -> None:
         self.__w_value = w_value
-    
-    def compute(self, expect_mean, expect_std, w = 0.5) -> pd.DataFrame:
-        df = {'Original Array' : self.original_array}
-        
+
+    def compute(self, expect_mean, expect_std, w=0.5, output_original=False) -> pd.DataFrame:
+        df = {'Received Array': self.original_array} if output_original else {}
+
         z_array = []
         for i in range(len(self.original_array)):
             z_array.append((self.original_array[i]-expect_mean)/expect_std)
-        
-        df.update({'Z Array' : z_array})
+
+        df.update({'Z Array': z_array})
         sH_array = [0]
         sL_array = [0]
         for i in range(len(self.original_array)-1):
@@ -25,25 +26,7 @@ class Cusum_cal :
             next_sL = sL_array[i] - z_array[i+1] - w
             sH_array.append(next_sH if next_sH > 0 else 0)
             sL_array.append(next_sL if next_sL > 0 else 0)
-             
-        df.update({'SH Array' : sH_array})
-        df.update({'SL Array' : sL_array})
-        return pd.DataFrame(data=df)
-    
-    def standardize(self) -> np.array:
-        o_array = self.original_array.copy()
-        mean = np.mean(o_array)
-        for i in range(len(o_array)):
-            o_array[i] = o_array[i] - mean
-        
-        o_std = np.std(o_array)
-        for i in range(len(o_array)):
-            o_array[i] = o_array[i]/(o_std/0.5)
-        
-        return o_array
 
-x = Cusum_cal([-0.60207, -0.85543, 0.4084, 0.60292, 0.14554, -0.11812, -0.22425, -0.25985, -0.26579, 0.70213, 0.32766, 0.55666, 0.50528, 0.62256, 0.21262, 0.36577, 0.85273, 1.0159, 0.53494, 1.4023])
-df = x.compute(0, 0.5, 0.5)
-print(df)
-df.plot()
-plt.show()
+        df.update({'SH Array': sH_array})
+        df.update({'SL Array': sL_array})
+        return pd.DataFrame(data=df)
---
+++ mental_disorder_cusum.py
@@ -0,0 +1,42 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from cusum import Cusum_cal
+
+
+csv_path = './mh/mental_disorder.csv'
+df = pd.read_csv(csv_path)
+country_names = set(df['location_name'].to_numpy())
+
+
+def main(country_name) -> None:
+
+    print(f"location_name == \"{country_name}\" and metric_name == \'Rate\'")
+    df_c = df.query(f"location_name == \'{country_name}\' and metric_name == \'Rate\'")[
+        ['year', 'val', 'upper', 'lower']].copy(deep=True)
+
+    vals = df_c['val'].to_numpy()
+    vals = vals/np.max(vals)
+    # Standarized by diving by max vals
+
+    vals_cusum = Cusum_cal(vals)
+    df_vals = vals_cusum.compute(
+        np.mean(vals[0:-2]), np.std(vals[0:-2]), output_original=True)
+
+    df_c_y_v = (df_c[['year', 'val']].copy(deep=True)).reset_index()
+    df_combined = df_c_y_v.join(df_vals)
+
+    val_fig = df_c.plot(x='year')
+    cusum_fig = df_vals.plot()
+    country_name_out = country_name.replace(' ', "_").replace(chr(92), '')
+    val_fig.get_figure().savefig(f'./output/{country_name_out}_md_values.png')
+    cusum_fig.get_figure().savefig(f'./output/{country_name_out}_md_cusum.png')
+    df_combined.to_csv(f'./output/{country_name_out}_md_cusum.csv')
+    print(f'{country_name_out} is done.')
+    plt.close()
+
+
+for i in country_names:
+    i = i.replace("'", "\\'")
+    main(i)
