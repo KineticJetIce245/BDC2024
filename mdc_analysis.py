e14009089aeb68b8a45fc93eb20ac94c31d455dc
Kineticjetice245 <kineticjetice245@gmail.com>
2024-07-14 18:25:51 -0400

    Compared cusum results

--- mdc_analysis.py
+++ mdc_analysis.py
@@ -10,11 +10,14 @@ remarkable_number = 0
 
 def check_if_remarkable(csv_path):
     df = pd.read_csv(csv_path)
-    print("Standard deviation: " + str(np.std(df['Received Array'])))
-    max_SH = np.max(df['SH Array'])
-    print('last' + str(df['SH Array']))
-    if (np.max(df['SH Array']) > 10):
+    sh_array = df['SH Array'].to_numpy()
+    five_sigma = 5*np.std(sh_array[:-2])
+    print(str(five_sigma) + " vs " +
+          str(sh_array[-1]) + " or " + str(sh_array[-2]))
+    if (five_sigma < sh_array[-1]) or (five_sigma < sh_array[-2]):
         return True
+    else:
+        print('stop')
 
 
 number_of_countries = 0
@@ -27,3 +30,4 @@ for f in files:
 
 print('number of countries: ' + str(number_of_countries))
 print('remarkable number: ' + str(remarkable_number))
+print('percentage: ' + str(remarkable_number/number_of_countries*100) + '%')
